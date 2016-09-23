e176a7b680054a19f637574ef025c4dda988d8b7
AntonioAzcona <tom.ramms.15@gmail.com>
2016-09-22 23:03:36 -0500

    ee_t04_listas

---
+++ ee_t04_listas/src/LinkedListException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Antonio
+ */
+class LinkedListException extends Exception {
+
+    LinkedListException(String la_lista_está_vacía) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
